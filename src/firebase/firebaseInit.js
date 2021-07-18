2a38069f8f30e1d8ad7d6204dd70cb29ee7bf8d1
karansonawane <sonawanekaran0007@gmail.com>
2021-07-18 20:06:22 +0530

    first commit

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBN1nqpvWQtcvk_OjvF5CYEMsP2t-L6_cE",
+    authDomain: "artbcozofchai.firebaseapp.com",
+    projectId: "artbcozofchai",
+    storageBucket: "artbcozofchai.appspot.com",
+    messagingSenderId: "1023093622656",
+    appId: "1:1023093622656:web:367abb42105c0d4357e365"
+  };
