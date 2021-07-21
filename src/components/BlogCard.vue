7e146395d9daa45e5077ea2c4238c1e7a2c2a50c
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-07-21 11:28:41 +0530

    Update BlogCard.vue

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="blog-card">
-    <div class="icons">
+    <div v-show="editPost" class="icons">
       <div class="icon">
         <Edit class="edit" />
       </div>
@@ -35,6 +35,11 @@ export default {
     Edit,
     Delete,
   },
+  computed: {
+    editPost() {
+      return this.$store.state.editPost;
+    },
+  },
 };
 </script>
 
@@ -56,90 +61,90 @@ export default {
   }
 
   .icons {
+    display: flex;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    z-index: 99;
+
+    .icon {
       display: flex;
-      position: absolute;
-      top: 10px;
-      right: 10px;
-      z-index: 99;
+      justify-content: center;
+      align-items: center;
+      width: 35px;
+      height: 35px;
+      border-radius: 50%;
+      background-color: #fff;
+      transition: 0.5s ease all;
 
-      .icon {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          width: 35px;
-          height: 35px;
-          border-radius: 50%;
-          background-color: #fff;
-          transition: .5s ease all;
+      &:hover {
+        background-color: #303030;
 
-          &:hover {
-            background-color: #303030;
-
-            .edit,
-            .delete {
-                path {
-                    fill: #fff;
-                }
-            }
+        .edit,
+        .delete {
+          path {
+            fill: #fff;
           }
-          &:nth-child(1) {
-              margin-right: 8px;
-          }
-          .edit,
-          .delete {
-              pointer-events: none;
-              height: 15px;
-              width: auto;
-          }
+        }
       }
+      &:nth-child(1) {
+        margin-right: 8px;
+      }
+      .edit,
+      .delete {
+        pointer-events: none;
+        height: 15px;
+        width: auto;
+      }
+    }
   }
 
   img {
-      display: block;
-      border-radius: 8px 8px 0 0;
-      z-index: 1;
-      width: 100%;
-      min-height: 200px;
-      object-fit: cover;
+    display: block;
+    border-radius: 8px 8px 0 0;
+    z-index: 1;
+    width: 100%;
+    min-height: 200px;
+    object-fit: cover;
   }
 
   .info {
-      display: flex;
-      flex-direction: column;
-      height: 100%;
-      z-index: 3;
-      padding: 32px 16px;
-      color: #000;
-      
-      h4 {
-          padding-bottom: 8px;
-          font-size: 20px;
-          font-weight: 300;
-      }
-      h6 {
-          font-weight: 400;
-          font-size: 12px;
-          padding-bottom: 16px;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    z-index: 3;
+    padding: 32px 16px;
+    color: #000;
+
+    h4 {
+      padding-bottom: 8px;
+      font-size: 20px;
+      font-weight: 300;
+    }
+    h6 {
+      font-weight: 400;
+      font-size: 12px;
+      padding-bottom: 16px;
+    }
+
+    .link {
+      display: inline-flex;
+      align-items: center;
+      margin-top: auto;
+      font-weight: 500;
+      padding-top: 20px;
+      font-style: 12px;
+      padding-bottom: 4px;
+      transition: 0.5s ease-in all;
+
+      &:hover {
+        color: rgba(48, 48, 0, 0.8);
       }
 
-      .link {
-          display: inline-flex;
-          align-items: center;
-          margin-top: auto;
-          font-weight: 500;
-          padding-top: 20px;
-          font-style: 12px;
-          padding-bottom: 4px;
-          transition: .5s ease-in all;
-
-          &:hover {
-              color: rgba(48, 48, 0, 0.8);
-          }
-
-          .arrow {
-              width: 10px;
-          }
+      .arrow {
+        width: 10px;
       }
+    }
   }
 }
 </style>
