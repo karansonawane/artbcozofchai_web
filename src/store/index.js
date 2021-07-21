f1ff4fdf49db60cdbe5a9cab29f25ce70985cfde
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-07-21 11:27:23 +0530

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,35 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    sampleBlogCard: [
+      {
+        blogTitle: "Blog Card #1",
+        blogCoverPhoto: "baby",
+        blogDate: "June 10, 2021",
+      },
+      {
+        blogTitle: "Blog Card #2",
+        blogCoverPhoto: "girl",
+        blogDate: "June 10, 2021",
+      },
+      {
+        blogTitle: "Blog Card #3",
+        blogCoverPhoto: "skull",
+        blogDate: "June 10, 2021",
+      },
+      {
+        blogTitle: "Blog Card #4",
+        blogCoverPhoto: "sparrow",
+        blogDate: "June 10, 2021",
+      },
+    ],
+    editPost: null,
   },
   mutations: {
+    toggleEditPost(state, payload) {
+      state.editPost = payload;
+      console.log(state.editPost);
+    }
   },
   actions: {
   },
