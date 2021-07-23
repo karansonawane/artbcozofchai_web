7da8cfd4ae99632654d32f70faac6175018dd9b1
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-07-23 12:45:24 +0530

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -17,11 +17,14 @@
           <input type="password" placeholder="password" v-model="password" />
           <password class="icon" />
         </div>
+        <div class="error" v-if="error">
+          {{ this.errorMsg }}
+        </div>
       </div>
       <router-link class="forgot-password" :to="{ name: 'ForgotPassword' }"
         >Forgot Your Password?</router-link
       >
-      <button>Sign In</button>
+      <button @click.prevent="signIn">Sign In</button>
       <div class="angle"></div>
     </form>
     <div class="background"></div>
@@ -31,6 +34,8 @@
 <script>
 import email from "../assets/Icons/envelope-regular.svg";
 import password from "../assets/Icons/lock-alt-solid.svg";
+import firebase from "firebase/app";
+import "firebase/auth";
 export default {
   name: "Login",
   components: {
@@ -41,7 +46,26 @@ export default {
     return {
       email: null,
       password: null,
+      error: null,
+      errorMsg: "",
     };
+  },
+  methods: {
+    signIn() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          this.$router.push({ name: "Home" });
+          this.error = false;
+          this.errorMsg = "";
+          console.log(firebase.auth().currentUser.uid);
+        })
+        .catch((err) => {
+          this.error = true;
+          this.errorMsg = err.message;
+        });
+    },
   },
 };
 </script>
