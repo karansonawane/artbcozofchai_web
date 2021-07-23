83ce019487e5e475a7d7f8b08be41a6a1e83c791
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-07-23 12:44:51 +0530

    Update firebaseInit.js

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,11 +1,17 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBN1nqpvWQtcvk_OjvF5CYEMsP2t-L6_cE",
-    authDomain: "artbcozofchai.firebaseapp.com",
-    projectId: "artbcozofchai",
-    storageBucket: "artbcozofchai.appspot.com",
-    messagingSenderId: "1023093622656",
-    appId: "1:1023093622656:web:367abb42105c0d4357e365"
-  };
+  apiKey: "AIzaSyBN1nqpvWQtcvk_OjvF5CYEMsP2t-L6_cE",
+  authDomain: "artbcozofchai.firebaseapp.com",
+  projectId: "artbcozofchai",
+  storageBucket: "artbcozofchai.appspot.com",
+  messagingSenderId: "1023093622656",
+  appId: "1:1023093622656:web:367abb42105c0d4357e365"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { timestamp };
+export default firebaseApp.firestore();
