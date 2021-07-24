db00c4e9cfccb3daf863df4ca7d9c600bfa848cd
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-07-24 12:51:30 +0530

    Update BlogPost.vue

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="blog-wrapper no-user">
+  <div class="blog-wrapper" :class="{ 'no-user': !user }">
     <div class="blog-content">
       <div>
         <h2 v-if="post.welcomeScreen">{{ post.title }}</h2>
@@ -37,6 +37,11 @@ export default {
   props: ["post"],
   components: {
     Arrow,
+  },
+  computed: {
+    user() {
+      return this.$store.state.user;
+    },
   },
 };
 </script>
@@ -149,7 +154,7 @@ export default {
 }
 
 .no-user:first-child {
-  .blog-content{
+  .blog-content {
     background-color: #303030;
     color: #ffff;
   }
