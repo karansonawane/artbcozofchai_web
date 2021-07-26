6cdbe1e33a849d3a9d6ce441b48efe163bdd5509
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-07-26 19:54:01 +0530

    Update Admin.vue

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,10 +1,97 @@
 <template>
-  <div>Admin</div>
+  <div class="admin">
+    <div class="container">
+      <h2>Administration</h2>
+      <div class="admin-info">
+        <h2>Add Admin</h2>
+        <div class="input">
+          <input
+            placeholder="Enter user email"
+            type="text"
+            id="addAdmins"
+            v-model="adminEmail"
+          />
+        </div>
+        <span>{{ this.functionMsg }}</span>
+        <button class="button">Submit</button>
+      </div>
+      <p>*This Panel is under construction</p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Admin",
+  data() {
+    return {
+      adminEmail: "",
+      functionMsg: null,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+.admin {
+  .container {
+    max-width: 1000px;
+    padding: 60px 25px;
+
+    p {
+      text-align: center;
+      color: red;
+    }
+
+    h2 {
+      text-align: center;
+      margin-bottom: 16px;
+      font-weight: 300;
+      font-size: 32px;
+    }
+
+    .admin-info {
+      border-radius: 8px;
+      box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+        0 2px 4px -1px rgba(0, 0, 0, 0.06);
+      padding: 32px;
+      background-color: #f1f1f1;
+      display: flex;
+      flex-direction: column;
+      max-width: 600px;
+      margin: 32px auto;
+
+      span {
+        font-size: 14px;
+      }
+
+      .input {
+        margin: 16px 0;
+
+        label {
+          font-size: 14px;
+          display: block;
+          padding-bottom: 6px;
+        }
+        input {
+          width: 100%;
+          border: none;
+          background-color: #f2f7f6;
+          padding: 8px;
+          height: 50px;
+          @media (min-width: 900px) {
+          }
+
+          &:focus {
+            outline: none;
+          }
+        }
+      }
+
+      button {
+        align-self: center;
+      }
+    }
+  }
+}
 </style>
