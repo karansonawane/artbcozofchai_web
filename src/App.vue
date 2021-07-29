90c98c8a4b9e82731fb35c3855f64b2f2fbd4a21
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-07-29 11:48:41 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -28,11 +28,11 @@ export default {
     firebase.auth().onAuthStateChanged((user) => {
       this.$store.commit("updateUser", user);
       if (user) {
-        this.$store.dispatch("getCurrentUser");
-        console.log(this.$store.state.profileEmail);
+        this.$store.dispatch("getCurrentUser", user);
       }
     });
     this.checkRoute();
+    this.$store.dispatch("getPost");
   },
   mounted() {},
   methods: {
