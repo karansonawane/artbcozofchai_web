f77ec8174e34fd16e03c122171a6607360d03cbb
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-07-29 19:23:53 +0530

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -52,6 +52,12 @@ export default new Vuex.Store({
       state.editPost = payload;
       console.log(state.editPost);
     },
+    setBlogState(state, payload) {
+      state.blogTitle = payload.blogTitle;
+      state.blogHTML = payload.blogHTML;
+      state.blogPhotoFileUrl = payload.blogCoverPhoto;
+      state.blogPhotoName = payload.blogCoverPhotoName;
+    },
     filterBlogPost(state, payload) {
       state.blogPosts = state.blogPosts.filter(post => post.blogID !== payload);
     },
@@ -98,11 +104,16 @@ export default new Vuex.Store({
             blogCoverPhoto: doc.data().blogCoverPhoto,
             blogTitle: doc.data().blogTitle,
             blogDate: doc.data().date,
+            blogCoverPhotoName: doc.data().blogCoverPhotoName,
           };
           state.blogPosts.push(data);
         }
       });
       state.postLoaded = true;
+    },
+    async updatePost({ commit, dispatch }, payload) {
+      commit('filterBlogPost', payload);
+      await dispatch("getPost");
     },
     async deletePost({ commit }, payload) {
       const getPost = await db.collection('blogPosts').doc(payload)
